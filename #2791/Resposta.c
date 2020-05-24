e197b0954472846051d71a81bc408f632222402b
Cayo Diebe <cayodiebe@gmail.com>
2020-05-24 13:10:38 -0300

    #2791

---
+++ #2791/Resposta.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    	int c1[4], i = 0; 
+	scanf("%i%i%i%i", &c1[0], &c1[1], &c1[2], &c1[3]); while(c1[i] !=1 && c1[i] == 0) 
+	i++; 
+	printf("%i\n", i+1); 
+	
+	return 0;
+}
